3fadf12196c7d45dbe6918ecae90958d1c15ac86
GilTads <gil.tads@gmail.com>
2016-09-28 17:58:05 -0400

    add tabs bootstrap na área de aulas

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -1,12 +1,23 @@
 $(document).ready(function(){
 
+	$('a[href="#main"]').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
 
+	$('a[href="#menu1"]').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
 
-	$('a[href="#main"]').tab('show');
+	$('a[href="#menu2"]').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
 
-	$('a[href="#menu1"]').tab('show');
+	$('a[href="#menu3"]').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
 
-	$('a[href="#menu2"]').tab('show');
-
-	$('a[href="#menu3"]').tab('show');
 });
